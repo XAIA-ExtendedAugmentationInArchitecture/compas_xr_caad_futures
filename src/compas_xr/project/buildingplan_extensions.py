951d537c4b1753e2761e5b5e1021c9fdb07ec90f
Eleni Alexi <elenialex96@gmail.com>
2025-06-29 21:16:50 -0400

    custom sequence and mixed data types

--- src/compas_xr/project/buildingplan_extensions.py
+++ src/compas_xr/project/buildingplan_extensions.py
@@ -10,7 +10,6 @@ class BuildingPlanExtensions(object):
     The BuildingPlanExtensions class provides additional functionalities to the :class:`~compas_timber.planning.BuildingPlan`
     by providing a way to create a buildling plan from an established assembly sequence.
     """
-
     # TODO: This makes the building plan in a very manual way
     # TODO: but this needs to be resolved in tandem with building plan revisions.
     def create_buildingplan_from_assembly_sequence(self, assembly, data_type, robot_keys, priority_keys_lists):
@@ -58,7 +57,7 @@ class BuildingPlanExtensions(object):
 
         return building_plan
 
-    def create_buildingplan_from_with_custom_sequence(self, assembly, sequenced_keys, data_type, robot_keys, priority_keys_lists):
+    def create_buildingplan_with_custom_sequence(self, assembly, sequenced_keys, data_type, robot_keys, priority_keys_lists):
         """
         Create a compas_timber.planning.BuildingPlan based on the sequence of the assembly parts.
 
@@ -69,7 +68,7 @@ class BuildingPlanExtensions(object):
         sequenced_keys : list of str
             List of keys that are intended to be built in the order provided.
         data_type : int
-            List index of which data type will be loaded on the application side [0: 'Cylinder', 1: 'Box', 2: 'ObjFile']
+            Index of which data type will be loaded on the application side [0: 'Cylinder', 1: 'Box', 2: 'ObjFile']
         robot_keys : list of str
             List of keys that are intended to be built by the robot.
         priority_keys_lists : list of list of str
@@ -103,6 +102,67 @@ class BuildingPlanExtensions(object):
                     step.actor = "HUMAN"
             else:
                 step.actor = "HUMAN"
+
+            if not priority_keys_lists:
+                step.priority = 0
+            else:
+                for i, keys_list in enumerate(priority_keys_lists):
+                    if key in keys_list:
+                        step.priority = i
+                        break
+            building_plan.add_step(step)
+
+        return building_plan
+    
+    def create_buildingplan_with_custom_sequence_and_mixed_data_types(self, assembly, sequenced_keys, robot_keys=None, priority_keys_lists=None):
+        """
+        Create a compas_timber.planning.BuildingPlan based on the sequence of the assembly parts.
+
+        Parameters
+        ----------
+        assembly : compas_timber.assembly.TimberAssembly or compas.datastructures.Assembly
+            The assembly that you want to generate the buiding plan for.
+        sequenced_keys : list of str
+            List of keys that are intended to be built in the order provided.
+        mixed_types : list of int
+            List index of which data type will be loaded on the application side [0: 'Cylinder', 1: 'Box', 2: 'ObjFile']
+        robot_keys : list of str
+            List of keys that are intended to be built by the robot.
+        priority_keys_lists : list of list of str
+            List in assembly order of lists of assembly keys that can be built in parallel.
+
+        Returns
+        -------
+        building_plan : compas_timber.planning.BuildingPlan
+            The building plan generated from the assembly sequence.
+
+        """
+        data_type_list = ["0.Cylinder", "1.Box", "2.ObjFile"]
+
+        graph_data = assembly.graph.__data__
+        node_data = graph_data["node"]
+        building_plan = BuildingPlan()
+
+        for key in sequenced_keys:
+            step = Step(key)
+            # TODO: This is dumb, but the element_ids are generated incorrectly so they are overwritten here
+            step.element_ids = [key]
+            step.geometry = node_data[str(key)]["part"].__data__["attributes"]["type"]
+            # TODO: These are unused for now, but are expeted on the application side
+            step.instructions = node_data[str(key)]["part"].__data__["attributes"].get("ObjName", "none")
+            step.elements_held = [0]
+            step.location = node_data[str(key)]["part"].frame
+
+            if robot_keys:
+                if key in robot_keys:
+                    step.actor = "ROBOT"
+                else:
+                    step.actor = "HUMAN"
+            else:
+                step.actor = "HUMAN"
+            
+            if "placed_by" in node_data[str(key)]["part"].__data__["attributes"]:
+                step.actor = node_data[str(key)]["part"].__data__["attributes"]["placed_by"]
 
             if not priority_keys_lists:
                 step.priority = 0
